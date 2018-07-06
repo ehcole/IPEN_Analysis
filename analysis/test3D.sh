64e4adec463763e60e0f8ba49790cece9c8a1051
ehcole <ehcole@umich.edu>
2018-07-06 16:13:57 -0400

    added attempt3/IPEN-012 to model exp 3

---
+++ analysis/test3D.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "3D model:"
+echo
+echo "criticality:"
+python /scratch/wrm_fluxoe/ehcole/IPEN_Analysis/analysis/k_eff.py /scratch/wrm_fluxoe/ehcole/IPEN_Analysis/version2/IPEN-008/IPEN-008.h5
+echo "power distribution:"
+python /scratch/wrm_fluxoe/ehcole/IPEN_Analysis/analysis/pinPowers.py /scratch/wrm_fluxoe/ehcole/IPEN_Analysis/version2/IPEN-011/IPEN-011.h5
+echo "reaction rate distribution:"
+python /scratch/wrm_fluxoe/ehcole/IPEN_Analysis/analysis/reactionRates.py /scratch/wrm_fluxoe/ehcole/IPEN_Analysis/version2/IPEN-008/IPEN-008.h5
+echo
+
